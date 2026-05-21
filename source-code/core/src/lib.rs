143a5d65bf1129fe9982097facd543215ce607a9
michal92299 <chudybyczek2137@gmail.com>
2026-05-21 16:21:45 +0200

    Update lib.rs

--- source-code/core/src/lib.rs
+++ source-code/core/src/lib.rs
@@ -1,9 +1,15 @@
 pub mod compositor;
 pub mod config;
+pub mod cursor;
+pub mod dmabuf;
 pub mod frame;
+pub mod input_handler;
 pub mod output;
+pub mod render;
 pub mod session;
 pub mod state;
+pub mod telemetry;
+pub mod window;
 pub mod xwayland;
 
 pub use config::Config;
